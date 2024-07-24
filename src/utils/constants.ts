31cfbbbcc3c42a9bf28c717d30e69414fb890d6f
zubastu <79757684+zubastu@users.noreply.github.com>
2024-07-25 01:38:39 +0300

    обавил мобильную версию для расчета зп

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001"; //"http://localhost:3001"
+export const BASE_URL = "https://api.idea-coffee.ru"; //"http://localhost:3001"
 
 export const nightHours = " 00:59:59:0000";
 export const dayHours = " 09:59:00:0001";
