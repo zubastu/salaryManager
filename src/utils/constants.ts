d90fe11f007dec1ace719bf40685c7f48849048f
zubastu <79757684+zubastu@users.noreply.github.com>
2024-07-27 15:58:48 +0300

    Добавил версию для портфолио

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001"; //"https://api.idea-coffee.ru"
+export const BASE_URL = "https://salary-api.ilya-makhin.ru"; //"https://api.idea-coffee.ru"
 
 export const nightHours = " 20:59:59:0000";
 export const dayHours = " 09:59:00:0001";
